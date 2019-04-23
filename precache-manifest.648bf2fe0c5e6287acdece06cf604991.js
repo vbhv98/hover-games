369d2d308f3f98ce651f66a3c49052583cf45d3f
aedit <udisen97@gmail.com>
2019-04-24 03:18:21 +0530

    Updates

---
+++ precache-manifest.648bf2fe0c5e6287acdece06cf604991.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e6e0c6e9d3685694b57",
+    "url": "/hovergames/static/css/main.85d53a80.chunk.css"
+  },
+  {
+    "revision": "1e6e0c6e9d3685694b57",
+    "url": "/hovergames/static/js/main.c82c5445.chunk.js"
+  },
+  {
+    "revision": "e979f67805239216f30d",
+    "url": "/hovergames/static/js/runtime~main.fb80d95e.js"
+  },
+  {
+    "revision": "abe6abf012cdd88e91cc",
+    "url": "/hovergames/static/css/2.0d5f71bc.chunk.css"
+  },
+  {
+    "revision": "abe6abf012cdd88e91cc",
+    "url": "/hovergames/static/js/2.a276ee8d.chunk.js"
+  },
+  {
+    "revision": "3214ddaae8c4e01986e138d4ef241e91",
+    "url": "/hovergames/static/media/Dodge.3214ddaa.png"
+  },
+  {
+    "revision": "e0e0eafb9aecb9715356e7aefc9b99d0",
+    "url": "/hovergames/static/media/Pong.e0e0eafb.png"
+  },
+  {
+    "revision": "a3f8cc26e932089f1a2c310872bdab22",
+    "url": "/hovergames/static/media/Breakout.a3f8cc26.png"
+  },
+  {
+    "revision": "0da1d476bb10b44429787523016618b9",
+    "url": "/hovergames/static/media/Snake.0da1d476.png"
+  },
+  {
+    "revision": "4965457ba1fadd504f1c71847d8a283b",
+    "url": "/hovergames/static/media/udit.4965457b.jpg"
+  },
+  {
+    "revision": "4918debe9c87b56ed0433c2ccebc65d3",
+    "url": "/hovergames/static/media/vinay.4918debe.jpg"
+  },
+  {
+    "revision": "851bedd4d55488366f40a680ec2ca047",
+    "url": "/hovergames/static/media/vaibhav.851bedd4.jpg"
+  },
+  {
+    "revision": "5632f659f8c57c79a807f06cf71471c6",
+    "url": "/hovergames/static/media/LoadImg.5632f659.webp"
+  },
+  {
+    "revision": "cb9bd8931c7b5560737d21e679f9296f",
+    "url": "/hovergames/index.html"
+  }
+];
